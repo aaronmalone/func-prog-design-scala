621c45ee4410a5a16f58c2260a7bb8f42f07e9e4
Aaron Malone <aaronmalone@gmail.com>
2016-11-27 13:53:47 -0500

    polynomial solver

--- calculator/src/main/scala/calculator/Polynomial.scala
+++ calculator/src/main/scala/calculator/Polynomial.scala
@@ -1,13 +1,22 @@
 package calculator
 
+import java.lang.Math.sqrt
+
 object Polynomial {
-  def computeDelta(a: Signal[Double], b: Signal[Double],
-      c: Signal[Double]): Signal[Double] = {
-    ???
-  }
+  def computeDelta(a: Signal[Double], b: Signal[Double], c: Signal[Double]): Signal[Double] =
+    Signal(b() * b() - 4 * a() * c())
 
   def computeSolutions(a: Signal[Double], b: Signal[Double],
-      c: Signal[Double], delta: Signal[Double]): Signal[Set[Double]] = {
-    ???
+                       c: Signal[Double], delta: Signal[Double]): Signal[Set[Double]] = {
+
+    def f(aa: Double, bb: Double, dd: Double): Set[Double] = {
+      if (dd < 0) Set.empty
+      else {
+        val s = sqrt(dd)
+        val twoA = 2 * aa
+        Set((-bb + s) / twoA, (-bb - s) / twoA)
+      }
+    }
+    Signal(f(a(), b(), delta()))
   }
 }
